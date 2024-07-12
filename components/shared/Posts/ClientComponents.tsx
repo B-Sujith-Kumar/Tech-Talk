d93abe908772c5436308293f41a9f97be130ec54
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-07-12 15:31:50 +0530

    FEAT: Added auth for voting on posts

--- components/shared/Posts/ClientComponents.tsx
+++ components/shared/Posts/ClientComponents.tsx
@@ -1,22 +1,33 @@
 "use client";
 
+import { useToast } from "@/components/ui/use-toast";
 import { downvotePost, upvotePost } from "@/lib/actions/user.actions";
 import { useUser } from "@clerk/nextjs";
 import { ArrowBigDownIcon, ArrowBigUpIcon } from "lucide-react";
 import mongoose from "mongoose";
 
-export const VotesButtons = ({ post }:{
+export const VotesButtons = ({ post }: {
     post: any;
 }) => {
     const { isLoaded, user } = useUser();
+    const { toast } = useToast();
     return <>
         <ArrowBigUpIcon className="w-6 h-6 cursor-pointer text-indigo-500"
             strokeWidth={1.1}
             onClick={async (e) => {
                 e.preventDefault();
+                if (isLoaded && !user) {
+                    toast({
+                        title: "Auth Error",
+                        description: "You need to be logged in to upvote a post.",
+                        variant: "destructive"
+                    });
+                    return;
+                }
                 await upvotePost({
                     postId: post._id?.toString() as string,
                 });
+                return;
             }}
             fill={
                 post.upvotes.findIndex(
@@ -32,9 +43,18 @@ export const VotesButtons = ({ post }:{
             strokeWidth={1.1}
             onClick={async (e) => {
                 e.preventDefault();
+                if (isLoaded && !user) {
+                    toast({
+                        title: "Auth Error",
+                        description: "You need to be logged in to downvote a post.",
+                        variant: "destructive"
+                    });
+                    return;
+                }
                 await downvotePost({
                     postId: post._id?.toString() as string,
                 });
+                return;
             }}
             fill={
                 post.downvotes.findIndex(
