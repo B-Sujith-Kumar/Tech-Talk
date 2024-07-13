a3b5341a477390ed00b6222213be9a1e560d688d
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-07-13 09:20:46 +0530

    FIX: Fixed error in post page for non-auth users

--- components/shared/Posts/Comments.tsx
+++ components/shared/Posts/Comments.tsx
@@ -3,11 +3,11 @@
 import { IPostPopulated } from "@/types";
 import React, { useEffect, useState } from "react";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
 } from "@/components/ui/select";
 import Image from "next/image";
 import { IUser } from "@/lib/database/models/user.model";
@@ -20,156 +20,160 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { UpDownVote } from "./UpDownVotePost";
 import { UpDownVoteComment } from "./UpDownVoteComment";
+import { useUser } from "@clerk/nextjs";
 
 const Comments = ({
-  post,
-  currentUser,
+    post,
+    currentUser,
 }: {
-  post: IPostPopulated;
-  currentUser: IUser;
+    post: IPostPopulated;
+    currentUser: IUser;
 }) => {
-  const [sort, setSort] = useState<"Top" | "Latest" | "Oldest">("Top");
-  const [content, setContent] = useState("");
-  const [comments, setComments] = useState(post.comments);
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
-  const { toast } = useToast();
+    const [sort, setSort] = useState<"Top" | "Latest" | "Oldest">("Top");
+    const [content, setContent] = useState("");
+    const [comments, setComments] = useState(post.comments);
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+    const { toast } = useToast();
+    const { isSignedIn } = useUser();
 
-  const handleChange = (value: string) => {
-    setSort(value as "Top" | "Latest" | "Oldest");
-  };
+    const handleChange = (value: string) => {
+        setSort(value as "Top" | "Latest" | "Oldest");
+    };
 
-  useEffect(() => {
-    setComments(post.comments);
-  }, [post.comments]);
+    useEffect(() => {
+        setComments(post.comments);
+    }, [post.comments]);
 
-  const handleSubmitComment = async () => {
-    setLoading(true);
-    try {
-      const { status, comment } = await addComment(
-        post._id!,
-        content,
-        currentUser._id?.toString()!
-      );
-      if (status === 200) {
-        toast({
-          title: "Comment posted",
-          description: "Your comment has been posted successfully",
-        });
-        setContent("");
-        setComments((prevComments) => [...prevComments, comment]);
-      }
-    } catch (error) {
-      toast({
-        title: "Sorry! An error occurred",
-        description: "An error occurred while posting the comment",
-      });
-    } finally {
-      setLoading(false);
-      //   router.refresh();
-    }
-  };
+    const handleSubmitComment = async () => {
+        setLoading(true);
+        try {
+            const { status, comment } = await addComment(
+                post._id!,
+                content,
+                currentUser._id?.toString()!
+            );
+            if (status === 200) {
+                toast({
+                    title: "Comment posted",
+                    description: "Your comment has been posted successfully",
+                });
+                setContent("");
+                setComments((prevComments) => [...prevComments, comment]);
+            }
+        } catch (error) {
+            toast({
+                title: "Sorry! An error occurred",
+                description: "An error occurred while posting the comment",
+            });
+        } finally {
+            setLoading(false);
+            //   router.refresh();
+        }
+    };
 
-  return (
-    <div className="px-2">
-      <div className="flex items-center mt-6 gap-3">
-        <h1 className="text-2xl font-semibold ">
-          {sort} comments ({comments.length})
-        </h1>
-        <Select onValueChange={handleChange}>
-          <SelectTrigger className="w-10 bg-white focus-visible:ring-0 focus-visible:ring-offset-0"></SelectTrigger>
-          <SelectContent className="bg-white px-2">
-            <h3 className="text-lg py-1 font-semibold">Sort comments:</h3>
-            <SelectItem
-              value="Top"
-              className="hover:bg-gray-300 cursor-pointer"
-            >
-              <p className="text-base font-medium">Top</p>
-              <p className="text-sm text-gray-600">
-                Most upvoted and relevant comments will be first
-              </p>
-            </SelectItem>
-            <SelectItem
-              value="Latest"
-              className="hover:bg-gray-300 cursor-pointer"
-            >
-              <p className="text-base font-medium">Latest</p>
-              <p className="text-sm text-gray-600">
-                Most recent comments will be first
-              </p>
-            </SelectItem>
-            <SelectItem
-              value="Oldest"
-              className="hover:bg-gray-300 cursor-pointer"
-            >
-              <p className="text-base font-medium">Oldest</p>
-              <p className="text-sm text-gray-600">
-                The oldest comments will be first
-              </p>
-            </SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
-      <div className="flex gap-4 items-start mt-6">
-        <Image
-          src={currentUser.profilePicture!}
-          width={42}
-          height={42}
-          alt="Profile picture"
-          className="aspect-square w-10 h-10 rounded-full mt-3"
-        />
-        <div className="flex flex-col flex-1 gap-3">
-          <Textarea
-            placeholder="Add a comment"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
-          <button
-            className={`text-sm ${
-              content.trim().length === 0
-                ? "bg-indigo-400 cursor-not-allowed"
-                : "bg-indigo-600 cursor-pointer"
-            } font-semibold text-white w-fit px-4 py-2 rounded-md`}
-            disabled={content.trim().length === 0}
-            onClick={handleSubmitComment}
-          >
-            {loading ? "Posting..." : "Post comment"}
-          </button>
-        </div>
-      </div>
-      {comments.map((comment: any) => (
-        <div key={comment._id?.toString()} className="mt-6">
-          <div className="flex gap-4 items-start">
-            <Image
-              src={comment.author.profilePicture}
-              width={42}
-              height={42}
-              alt="Profile picture"
-              className="aspect-square w-10 h-10 rounded-full mt-3"
-            />
-            <div className="flex flex-col gap-3 flex-1">
-              <div className="flex flex-col flex-1 gap-3 border px-3 py-4 rounded-lg">
-                <div className="flex items-center">
-                  <Link
-                    href={`/user/${comment.author._id}`}
-                    className="font-semibold"
-                  >
-                    {comment.author.firstName + " " + comment.author.lastName}
-                  </Link>
-                  <Dot className="text-gray-600" />
-                  <p className="text-gray-500 text-sm">
-                    {moment(comment.createdAt).fromNow()}
-                  </p>
-                </div>
-                <p className="text-base">{comment.content}</p>
-              </div>
-              <UpDownVoteComment comment={comment} currentUser={currentUser} />
+    return (
+        <div className="px-2">
+            <div className="flex items-center mt-6 gap-3">
+                <h1 className="text-2xl font-semibold ">
+                    {sort} comments ({comments.length})
+                </h1>
+                <Select onValueChange={handleChange}>
+                    <SelectTrigger className="w-10 bg-white focus-visible:ring-0 focus-visible:ring-offset-0"></SelectTrigger>
+                    <SelectContent className="bg-white px-2">
+                        <h3 className="text-lg py-1 font-semibold">Sort comments:</h3>
+                        <SelectItem
+                            value="Top"
+                            className="hover:bg-gray-300 cursor-pointer"
+                        >
+                            <p className="text-base font-medium">Top</p>
+                            <p className="text-sm text-gray-600">
+                                Most upvoted and relevant comments will be first
+                            </p>
+                        </SelectItem>
+                        <SelectItem
+                            value="Latest"
+                            className="hover:bg-gray-300 cursor-pointer"
+                        >
+                            <p className="text-base font-medium">Latest</p>
+                            <p className="text-sm text-gray-600">
+                                Most recent comments will be first
+                            </p>
+                        </SelectItem>
+                        <SelectItem
+                            value="Oldest"
+                            className="hover:bg-gray-300 cursor-pointer"
+                        >
+                            <p className="text-base font-medium">Oldest</p>
+                            <p className="text-sm text-gray-600">
+                                The oldest comments will be first
+                            </p>
+                        </SelectItem>
+                    </SelectContent>
+                </Select>
             </div>
-          </div>
+            <div className="flex gap-4 items-start mt-6">
+                {isSignedIn &&
+                    <Image
+                        src={currentUser.profilePicture!}
+                        width={42}
+                        height={42}
+                        alt="Profile picture"
+                        className="aspect-square w-10 h-10 rounded-full mt-3"
+                    />}
+                <div className="flex flex-col flex-1 gap-3">
+                    <Textarea
+                        placeholder={isSignedIn ? "Write a comment..." : "Sign in to comment"}
+                        value={content}
+                        disabled={!isSignedIn}
+                        onChange={(e) => setContent(e.target.value)}
+                        className="placeholder-gray-500"
+                    />
+                    {isSignedIn && <button
+                        className={`text-sm ${content.trim().length === 0
+                            ? "bg-indigo-400 cursor-not-allowed"
+                            : "bg-indigo-600 cursor-pointer"
+                            } font-semibold text-white w-fit px-4 py-2 rounded-md`}
+                        disabled={content.trim().length === 0}
+                        onClick={handleSubmitComment}
+                    >
+                        {loading ? "Posting..." : "Post comment"}
+                    </button>}
+                </div>
+            </div>
+            {comments.map((comment: any) => (
+                <div key={comment._id?.toString()} className="mt-6">
+                    <div className="flex gap-4 items-start">
+                        <Image
+                            src={comment.author.profilePicture}
+                            width={42}
+                            height={42}
+                            alt="Profile picture"
+                            className="aspect-square w-10 h-10 rounded-full mt-3"
+                        />
+                        <div className="flex flex-col gap-3 flex-1">
+                            <div className="flex flex-col flex-1 gap-3 border px-3 py-4 rounded-lg">
+                                <div className="flex items-center">
+                                    <Link
+                                        href={`/user/${comment.author._id}`}
+                                        className="font-semibold"
+                                    >
+                                        {comment.author.firstName + " " + comment.author.lastName}
+                                    </Link>
+                                    <Dot className="text-gray-600" />
+                                    <p className="text-gray-500 text-sm">
+                                        {moment(comment.createdAt).fromNow()}
+                                    </p>
+                                </div>
+                                <p className="text-base">{comment.content}</p>
+                            </div>
+                            <UpDownVoteComment comment={comment} currentUser={currentUser} />
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default Comments;
--- lib/actions/post.action.ts
+++ lib/actions/post.action.ts
@@ -10,171 +10,184 @@ import Comment from "../database/models/comment.model";
 import { revalidatePath } from "next/cache";
 
 export async function createPost(data: {
-  title: string;
-  content: string;
-  tags?: string[];
-  community?: mongoose.Schema.Types.ObjectId | string;
-  coverImage?: string;
+    title: string;
+    content: string;
+    tags?: string[];
+    community?: mongoose.Schema.Types.ObjectId | string;
+    coverImage?: string;
 }) {
-  try {
-    let auth = isAuth();
-    if (!auth)
-      return {
-        status: 401,
-        message: "Unauthorized",
-      };
-    const user = await currentUser();
-    await connectToDatabase();
-    let post: IPost;
-    if (
-      data.community === "public" ||
-      !data.community ||
-      data.community === ""
-    ) {
-      post = await Post.create({
-        author: user?.publicMetadata.userId,
-        title: data.title,
-        content: data.content,
-        coverImage: data.coverImage,
-      });
-    } else {
-      post = await Post.create({
-        author: user?.publicMetadata.userId,
-        title: data.title,
-        content: data.content,
-        community: data.community as mongoose.Schema.Types.ObjectId,
-        coverImage: data.coverImage,
-      });
-    }
-    await Promise.all(
-      data.tags?.forEach(async (tag) => {
-        let existingTag: ITag | null = await Tag.findOne({ name: tag });
-        if (!existingTag) {
-          let newTag: ITag = await Tag.create({
-            name: tag,
-            posts: [post._id],
-          });
-          post.tags.push(newTag._id as mongoose.Schema.Types.ObjectId);
+    try {
+        let auth = isAuth();
+        if (!auth)
+            return {
+                status: 401,
+                message: "Unauthorized",
+            };
+        const user = await currentUser();
+        await connectToDatabase();
+        let post: IPost;
+        if (
+            data.community === "public" ||
+            !data.community ||
+            data.community === ""
+        ) {
+            post = await Post.create({
+                author: user?.publicMetadata.userId,
+                title: data.title,
+                content: data.content,
+                coverImage: data.coverImage,
+            });
         } else {
-          existingTag?.posts?.push(post._id as mongoose.Schema.Types.ObjectId);
-          post.tags.push(existingTag._id as mongoose.Schema.Types.ObjectId);
-          await existingTag.save();
+            post = await Post.create({
+                author: user?.publicMetadata.userId,
+                title: data.title,
+                content: data.content,
+                community: data.community as mongoose.Schema.Types.ObjectId,
+                coverImage: data.coverImage,
+            });
         }
-      }) ?? []
-    );
-    await post.save();
-    return { status: 200, message: "Post created successfully" };
-  } catch (error: any) {
-    return { status: 500, message: error.message };
-  }
+        await Promise.all(
+            data.tags?.forEach(async (tag) => {
+                let existingTag: ITag | null = await Tag.findOne({ name: tag });
+                if (!existingTag) {
+                    let newTag: ITag = await Tag.create({
+                        name: tag,
+                        posts: [post._id],
+                    });
+                    post.tags.push(newTag._id as mongoose.Schema.Types.ObjectId);
+                } else {
+                    existingTag?.posts?.push(post._id as mongoose.Schema.Types.ObjectId);
+                    post.tags.push(existingTag._id as mongoose.Schema.Types.ObjectId);
+                    await existingTag.save();
+                }
+            }) ?? []
+        );
+        await post.save();
+        return { status: 200, message: "Post created successfully" };
+    } catch (error: any) {
+        return { status: 500, message: error.message };
+    }
 }
 
-export async function getAllPosts() {
-  try {
-    await connectToDatabase();
-    let posts = await Post.find()
-      .populate("tags")
-      .populate("author")
-      .populate("community");
-    return { status: 200, data: JSON.parse(JSON.stringify(posts)) };
-  } catch (error: any) {
-    return { status: 500, message: error.message };
-  }
+export async function getAllPosts(options:
+    { limit: number; skip: number; sort: string; order: string; } = {
+        limit: 10,
+        skip: 0,
+        sort: "createdAt",
+        order: "desc",
+    }
+) {
+    try {
+        await connectToDatabase();
+        let posts = await Post.find()
+            .populate("tags")
+            .populate("author")
+            .populate("community")
+            .limit(options.limit)
+            .skip(options.skip)
+            .sort({
+                createdAt: options.order === "desc" ? -1 : 1,
+            })
+        return { status: 200, data: JSON.parse(JSON.stringify(posts)) };
+    }
+    catch (error: any) {
+        return { status: 500, message: error.message };
+    }
 }
 
 export async function getPostById(postID: mongoose.Schema.Types.ObjectId) {
-  try {
-    await connectToDatabase();
-    let post = await Post.findById(postID)
-      .populate("tags")
-      .populate("author")
-      .populate("community")
-      .populate({
-        path: "comments",
-        populate: {
-          path: "author",
-        },
-      });
-    return JSON.parse(JSON.stringify({ status: 200, data: post }));
-  } catch (error: any) {
-    return { status: 500, message: error.message };
-  }
+    try {
+        await connectToDatabase();
+        let post = await Post.findById(postID)
+            .populate("tags")
+            .populate("author")
+            .populate("community")
+            .populate({
+                path: "comments",
+                populate: {
+                    path: "author",
+                },
+            });
+        return JSON.parse(JSON.stringify({ status: 200, data: post }));
+    } catch (error: any) {
+        return { status: 500, message: error.message };
+    }
 }
 
 export const addComment = async (
-  postID: mongoose.Schema.Types.ObjectId,
-  content: string,
-  userID: string
+    postID: mongoose.Schema.Types.ObjectId,
+    content: string,
+    userID: string
 ) => {
-  try {
-    await connectToDatabase();
-    let post = await Post.findById(postID);
-    if (!post)
-      return JSON.parse(
-        JSON.stringify({ status: 404, message: "Post not found" })
-      );
-    let comment = await Comment.create({
-      author: userID,
-      post: postID,
-      content: content,
-    });
-    let newComment = await Comment.findOne({ _id: comment._id }).populate(
-      "author"
-    );
-    post.comments.push(comment._id);
-    await post.save();
-    return JSON.parse(JSON.stringify({ status: 200, comment: newComment }));
-  } catch (error: any) {
-    return JSON.parse(JSON.stringify({ status: 500, message: error.message }));
-  }
+    try {
+        await connectToDatabase();
+        let post = await Post.findById(postID);
+        if (!post)
+            return JSON.parse(
+                JSON.stringify({ status: 404, message: "Post not found" })
+            );
+        let comment = await Comment.create({
+            author: userID,
+            post: postID,
+            content: content,
+        });
+        let newComment = await Comment.findOne({ _id: comment._id }).populate(
+            "author"
+        );
+        post.comments.push(comment._id);
+        await post.save();
+        return JSON.parse(JSON.stringify({ status: 200, comment: newComment }));
+    } catch (error: any) {
+        return JSON.parse(JSON.stringify({ status: 500, message: error.message }));
+    }
 };
 
 export const upVoteComment = async (
-  commentId: mongoose.Schema.Types.ObjectId,
-  userID: string
+    commentId: mongoose.Schema.Types.ObjectId,
+    userID: string
 ) => {
-  try {
-    await connectToDatabase();
-    let comment = await Comment.findById(commentId);
-    if (!comment)
-      return JSON.parse(
-        JSON.stringify({ status: 404, message: "Comment not found" })
-      );
-    if (comment.upvotes.includes(userID)) {
-      comment.upvotes = comment.upvotes.filter((id: any) => id.toString() !== userID.toString());
-    } else {
-      comment.upvotes.push(userID);
-      comment.downvotes = comment.downvotes.filter((id: any) => id.toString() !== userID.toString());
+    try {
+        await connectToDatabase();
+        let comment = await Comment.findById(commentId);
+        if (!comment)
+            return JSON.parse(
+                JSON.stringify({ status: 404, message: "Comment not found" })
+            );
+        if (comment.upvotes.includes(userID)) {
+            comment.upvotes = comment.upvotes.filter((id: any) => id.toString() !== userID.toString());
+        } else {
+            comment.upvotes.push(userID);
+            comment.downvotes = comment.downvotes.filter((id: any) => id.toString() !== userID.toString());
+        }
+        await comment.save();
+        revalidatePath(`/post/${comment.post}`)
+        return JSON.parse(JSON.stringify({ status: 200, message: "Success" }));
+    } catch (error: any) {
+        return JSON.parse(JSON.stringify({ status: 500, message: error.message }));
     }
-    await comment.save();
-    revalidatePath(`/post/${comment.post}`)
-    return JSON.parse(JSON.stringify({ status: 200, message: "Success" }));
-  } catch (error: any) {
-    return JSON.parse(JSON.stringify({ status: 500, message: error.message }));
-  }
 };
 
 export const downVoteComment = async (
-  commentId: mongoose.Schema.Types.ObjectId,
-  userID: string
+    commentId: mongoose.Schema.Types.ObjectId,
+    userID: string
 ) => {
-  try {
-    await connectToDatabase();
-    let comment = await Comment.findById(commentId);
-    if (!comment)
-      return JSON.parse(
-        JSON.stringify({ status: 404, message: "Comment not found" })
-      );
-    if (comment.downvotes.includes(userID)) {
-      comment.downvotes = comment.downvotes.filter((id: any) => id.toString() !== userID.toString());
-    } else {
-      comment.downvotes.push(userID);
-      comment.upvotes = comment.upvotes.filter((id: any) => id.toString() !== userID.toString());
+    try {
+        await connectToDatabase();
+        let comment = await Comment.findById(commentId);
+        if (!comment)
+            return JSON.parse(
+                JSON.stringify({ status: 404, message: "Comment not found" })
+            );
+        if (comment.downvotes.includes(userID)) {
+            comment.downvotes = comment.downvotes.filter((id: any) => id.toString() !== userID.toString());
+        } else {
+            comment.downvotes.push(userID);
+            comment.upvotes = comment.upvotes.filter((id: any) => id.toString() !== userID.toString());
+        }
+        await comment.save();
+        revalidatePath(`/post/${comment.post}`);
+        return JSON.parse(JSON.stringify({ status: 200, message: "Success" }));
+    } catch (error: any) {
+        return JSON.parse(JSON.stringify({ status: 500, message: error.message }));
     }
-    await comment.save();
-    revalidatePath(`/post/${comment.post}`);
-    return JSON.parse(JSON.stringify({ status: 200, message: "Success" }));
-  } catch (error: any) {
-    return JSON.parse(JSON.stringify({ status: 500, message: error.message }));
-  }
 };
